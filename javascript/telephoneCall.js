d77c0f3038e5f86a8836a2c60499ee78e841b8ca
elin57 <112911997+elin57@users.noreply.github.com>
2024-04-19 23:51:56 +0200

    Beginning of telephone page is made

--- javascript/telephoneCall.js
+++ javascript/telephoneCall.js
@@ -1,5 +1,45 @@
 "use strict";
 
 function renderTelephonePage(event) {
+    document.querySelector("body").innerHTML = "";
+    let  telephonePageBackground = document.createElement("div");
+    telephonePageBackground.setAttribute("id", "telephonePageBackground");
+    document.querySelector("body").appendChild(telephonePageBackground);
 
+    let topQuestionButton = document.createElement("div");
+    topQuestionButton.setAttribute("id", "topQuestionButtonTelephonePage");
+    topQuestionButton.textContent = "?";
+    telephonePageBackground.appendChild(topQuestionButton);
+
+    topQuestionButton.addEventListener("click", explainHowToListen);
+
+    let landlineTelephone = document.createElement("div");
+    landlineTelephone.setAttribute("id", "landlineTelephone");
+    telephonePageBackground.appendChild(landlineTelephone);
+}
+
+function explainHowToListen(event) {
+    if(!document.querySelector("div#overlayTelephonePage")) {
+        let overlay = document.createElement("div");
+        overlay.setAttribute("id", "overlayTelephonePage");
+        document.querySelector("body").appendChild(overlay);
+        document.querySelector("div#topQuestionButtonTelephonePage").style.zIndex = 1;
+
+        let infoContainer = document.createElement("div");
+        infoContainer.setAttribute("id", "infoContainerTelephonePage");
+        overlay.appendChild(infoContainer);
+        let infoContainerOverlay = document.createElement("div");
+        infoContainerOverlay.setAttribute("id", "infoContainerOverlay");
+
+        infoContainer.innerHTML = 
+        `
+        <p id="textInfoBoxTelephonePage">Tryck på telefonen <br>  
+        för att avlyssna röstmeddelandet</p> 
+        `;
+
+        infoContainer.appendChild(infoContainerOverlay);
+
+    } else {
+        document.querySelector("div#overlayTelephonePage").remove();
+    }
 }
