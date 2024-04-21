8a152ecfadc1273b3a3501fdb009c8ef653c46ef
elin57 <112911997+elin57@users.noreply.github.com>
2024-04-21 14:34:19 +0200

    Deleted stuff I don't need

--- Abbe/main.js
+++
@@ -1,8 +0,0 @@
-function submitUsername() {
-    const username = document.getElementById('username').value;
-    if (username) {
-        window.location.href = 'welcome.html?username=' + encodeURIComponent(username);
-    } else {
-        alert('Vänligen ange ett användarnamn.');
-    }
-}
