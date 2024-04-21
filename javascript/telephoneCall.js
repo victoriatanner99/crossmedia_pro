ffac4bd70ef5a5a24844925859de8bd004fa142f
elin57 <112911997+elin57@users.noreply.github.com>
2024-04-21 14:29:11 +0200

    Added a function

--- javascript/telephoneCall.js
+++ javascript/telephoneCall.js
@@ -43,3 +43,22 @@ function explainHowToListen(event) {
         document.querySelector("div#overlayTelephonePage").remove();
     }
 }
+
+function telephoneStartsToRing() {
+    document.addEventListener('DOMContentLoaded', function() {
+        const audio = document.getElementById('audioPlayer');
+        const vibratingIcon = document.getElementById('vibratingIcon');
+    
+        // Startar vibrations-effekten när sidan laddas
+        vibratingIcon.classList.add('vibrating');
+    
+        // Start playing the audio immediately after the page loads
+        audio.play();
+    
+     
+            vibratingIcon.addEventListener('click', () => {
+                window.location.href = './karin_text1.html'; 
+            });
+        
+    });
+}
