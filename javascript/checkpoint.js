b00751d8828aa4194eee5f26195382ddb51c40a6
Abbe96 <113461041+Abbe96@users.noreply.github.com>
2024-04-22 09:07:28 +0200

    ny

--- javascript/checkpoint.js
+++ javascript/checkpoint.js
@@ -1,7 +1,7 @@
 function checkInput() {
     var input = document.getElementById('feedbackInput').value;
     if (input === 'G10') {
-        window.location.href = 'success.html'; // Rätt kod, skicka till success-sidan
+        window.location.href = './Rebus.html'; // Rätt kod, skicka till success-sidan
     } else {
         alert('Fel kod, försök igen.'); // Felaktig kod, visa ett felmeddelande
     }
